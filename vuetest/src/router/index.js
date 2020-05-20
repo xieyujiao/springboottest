89844b9705abb2d823861b2baef8e3d3cc7a70ac
xieyujiao <736354146@qq.com>
2020-05-20 16:45:45 +0800

    前后端分离的前端的一个测试demo

---
+++ vuetest/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Book from '../views/Book.vue'//导入vue
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        //配置映射
+        path: '/book',
+        component: Book
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ vuetest/src/views/Book.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>编号</td>
+                <td>图书名称</td>
+                <td>作者</td>
+            </tr>
+            <tr v-for="item in books">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.author}}</td>
+            </tr>
+        </table>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Book",
+        data(){
+            return {
+            //定义的json数据
+              msg:'Hello Vue',
+              books:[
+                  {
+                      id:1,
+                      name:'theo',
+                      author:'贝贝'
+                  },
+                  {
+                      id:2,
+                      name:'justin',
+                      author:'富贵'
+                  },
+                  {
+                      id:3,
+                      name:'bill',
+                      author:'雯珺'
+                  },
+              ]
+            }
+        },
+        created() {
+            const _this=this
+            axios.get('http://localhost:8181/book/findAll').then(function(resp){
+                _this.books=resp.data
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
